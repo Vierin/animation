41146170db88b70fc94207c73e0d763748bc8eb4
Ivan_Vierin <ivan@huncwot.com>
2023-03-13 14:55:33 +0100

    fix color + easing

--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -18,36 +18,38 @@ class Main {
     private circlesCount: number;
     private circleStep: number;
     private particlesCount: number;
-    private particleSize: number;
-    private particleStep: number;
     private itemRadius: number;
     private startPositionX: number;
     private speed: number;
+    private test: number;
     private dotSize: number;
+    private colors: string[];
 
     constructor() {
         this.view = document.querySelector('.js-animation');
 
         // settings
-        this.itemsCount = 2;
+        this.itemsCount = 1;
         this.itemRadius = 200;
         this.speed = 0.001;
         
         
         this.circleRadius = 150;
-        this.circlesCount = 20;
+        this.circlesCount = 20; // 20
         this.circleStep = 360 / this.circlesCount;
         
 
-        this.particlesCount = 20;
-        this.particleSize = 10;
-        this.particleStep = 360 / this.particlesCount;
+        this.particlesCount = 2; //20
 
         this.dotSize = 5;
 
         this.startPositionX = window.innerWidth * 0.5;
         
-        const palete = new ColorPalete(this.particlesCount, "#FFEF40", "#424242");
+        // const palete = new ColorPalete(this.circlesCount, "#FFEF40", "#8E8955");
+        this.colors = ["FFEF40","FFEF40","FFEF40","FFEF40","F2E340","E4D640","D7CA40","C9BE41","BCB141","AEA541","A19941","938C41","868041","787341","6B6742","5D5B42","504E42","424242"];
+
+
+        
         
         this.initCanvas();
     }
@@ -82,12 +84,16 @@ class Main {
 
     private loop = (): void => {
         this.speed++
-
+         // easing
+        this.test = this.speed % 360 + (1 + Math.sin((90 + this.speed) / 180 * Math.PI)) * 50;     
+        
         this.ctx.clearRect(0, 0, this.canvas.width * this.pixelRatio, this.canvas.height * this.pixelRatio);
 
         for (let i = 1; i <= this.itemsCount; i++) {
             this.drawItem(i);
         }
+
+        
         
         this.raf = window.requestAnimationFrame(this.loop);
         
@@ -109,7 +115,9 @@ class Main {
     private drawItem(count?: number): void {
         // draw circles = single item
         for (let j = 0; j < this.circlesCount; j++) {
-            let angle = 125 + j * this.circleStep;
+            const test = this.speed % 360 + (1 + Math.sin((90 + this.speed + j) / 180 * Math.PI)) * 50;
+            let angle = 125 + j * this.circleStep + test;
+           
             
             let x = this.startPositionX + (this.itemRadius * (2 * (count - 1)))
             
@@ -120,17 +128,14 @@ class Main {
                 y:  y + this.itemRadius * Math.sin(angle * Math.PI / 180)
             }
 
-            this.drawCirle(obj);
+            this.drawCirle(obj, j);
         }
     }
 
-    private drawCirle(centerForCircle: {x: number, y: number}): void {
+    private drawCirle(centerForCircle: {x: number, y: number}, circleId: number): void {
         // draw 1 circle
         for (let i = 0; i < this.particlesCount; i++) {
-            let angle = ((this.speed) + i * this.circleStep);
-            // let angle = (i * this.circleStep);
-            // easing
-            angle -= Math.sin((90 + angle) / 180 * Math.PI) * 100 * this.circleStep / 150;
+            let angle = (i * this.circleStep);
           
             const x = this.itemsCount > 1 
             ? 
@@ -143,62 +148,49 @@ class Main {
                 y: centerForCircle.y + Math.cos(angle * Math.PI / 180)
             };
 
-            this.drawParticle(centerForParticles, i, angle);
+            this.drawParticle(centerForParticles, angle, circleId);
         } 
     }
 
 
-    private drawParticle(centerForParticles: {x: number; y: number}, i: number, angle: number): void {
+    private drawParticle(centerForParticles: {x: number; y: number}, angle: number, circleId: number): void {
         // draw each particle
         const x = centerForParticles.x + this.circleRadius * Math.cos(angle * Math.PI / 180);
         const y = centerForParticles.y + this.circleRadius * Math.sin(angle * Math.PI / 180);
         
-        this.drawOneParticle(x, y, i)
+        this.drawOneParticle(x, y, circleId);
     }
 
-    private drawOneParticle(x: number, y: number, id: number): void {
+    private drawOneParticle(x: number, y: number, circleId: number): void {
         
-        const size = this.dotSize - (this.dotSize / this.particlesCount) * id;
-        
-        const offsetX = size * 2;
-        const offsetY1 = offsetX * .82;
-        const offsetX2 = offsetX * .64;
+        const size = this.dotSize - (this.dotSize / this.circlesCount) * circleId;
+        const dotsCount = 5;
+        const particleRadius = dotsCount * 3;
+        const radius = particleRadius - (particleRadius / this.circlesCount) * circleId;
+        const test = this.speed % 360 + (1 + Math.sin((90 + this.speed + circleId) / 180 * Math.PI)) * 50;
+
+         
         
         this.ctx.beginPath();
-        // 1 
-        this.ctx.arc(
-            x,
-            y,
-            size, 
-            0, Math.PI * 2);
-        // 2
-        this.ctx.moveTo(x + offsetX, y + 8);
-        this.ctx.arc(
-            x + offsetX, 
-            y + offsetY1, 
-            size, 
-            0, Math.PI * 2); 
-        // 3
-        this.ctx.moveTo(x - offsetX, y + 8);
-        this.ctx.arc(
-            x - offsetX, 
-            y + offsetY1, 
-            size, 
-            0, Math.PI * 2); 
-        // 4 
-        this.ctx.moveTo(x - offsetX2, y + offsetX * 2.1);
-        this.ctx.arc(
-            x - offsetX2, 
-            y + offsetX * 2.1, 
-            size, 
-            0, Math.PI * 2);  
-        // 5
-        this.ctx.moveTo(x + offsetX2, y + offsetX * 2.1);
-        this.ctx.arc(
-            x + offsetX2, 
-            y + offsetX * 2.1, 
-            size, 
-            0, Math.PI * 2); 
+        const dotStep = 360 / dotsCount;
+        // colors  
+        this.ctx.fillStyle = `#${this.colors[circleId]}`;
+        
+        // draw dot 
+        for (let i = 0; i < dotsCount; i++) {
+           
+            const xCenter = x + radius * Math.cos((i * dotStep + test) * Math.PI / 180);
+            const yCenter = y + radius * Math.sin((i * dotStep + test) * Math.PI / 180);
+
+            this.ctx.moveTo(x, y);
+            this.ctx.arc(
+                xCenter,
+                yCenter,
+                size, 
+                0, Math.PI * 2);
+        }
+
+        //  end draw dot
 
         this.ctx.fill();
     }
